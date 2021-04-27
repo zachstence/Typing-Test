070cc4f55239c173cdc7bff412bd56355f32354b
zach_08 <zach08dev@gmail.com>
2021-04-26 20:03:13 -0500

    Create word component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,67 @@
-<script>
+<script lang="ts">
+	import Word from "./Word.svelte";
 
+	let words = [
+		"bother",
+		"elephant",
+		"panel",
+		"disability",
+		"inch",
+		"pyramid",
+		"separate",
+		"nationalism",
+		"live",
+		"rush",
+		"disappointment",
+		"salt",
+		"file",
+		"substitute",
+		"green",
+		"assignment",
+		"garage",
+		"horse",
+		"default",
+		"favorable",
+		"penetrate",
+		"version",
+		"velvet",
+		"allocation",
+		"pleasure",
+		"testify",
+		"touch",
+		"functional",
+		"Koran",
+		"direction"
+	];
+	let typed = "";
+	let t = 0;
+	
+	function onInput(e) {
+		console.log(e);
+		const toType = words[t];
+		
+		if (e.data === " ") {
+			const word = typed.trim();
+			typed = "";
+			if (word === toType) {
+				console.log(word);
+				t++;
+			} else {
+				console.log("mistake");
+			}
+		}
+	}
 </script>
 
-<main>
-
-</main>
 
 <style>
 
 </style>
+
+
+{#each words as word}
+	<Word word={word} correct={[]} />
+{/each}
+<input bind:value={typed} on:input={onInput}>
+<p>Typed: {typed}</p>
+
---
+++ src/Word.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let word: string;
+    export let correct: Boolean[];
+</script>
+
+
+<style>
+
+</style>
+
+
+{#each word as c, i}
+    <span class={correct[i] ? "green" : "red"}>{c}</span>
+{/each}
