859503236dee2df2f05339f6a108cdc9ca94852b
zach_08 <zach08dev@gmail.com>
2021-06-10 17:46:05 -0500

    Track words/letters typed with mistakes

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,108 @@
 <script lang="ts">
-	import Word from "./Word.svelte";
+    import Word from "./Word.svelte";
 
-	let words = [
-		"bother",
-		"elephant",
-		"panel",
-		"disability",
-		"inch",
-		"pyramid",
-		"separate",
-		"nationalism",
-		"live",
-		"rush",
-		"disappointment",
-		"salt",
-		"file",
-		"substitute",
-		"green",
-		"assignment",
-		"garage",
-		"horse",
-		"default",
-		"favorable",
-		"penetrate",
-		"version",
-		"velvet",
-		"allocation",
-		"pleasure",
-		"testify",
-		"touch",
-		"functional",
-		"Koran",
-		"direction"
-	];
-	let typed = "";
-	let t = 0;
-	
-	function onInput(e) {
-		console.log(e);
-		const toType = words[t];
-		
-		if (e.data === " ") {
-			const word = typed.trim();
-			typed = "";
-			if (word === toType) {
-				console.log(word);
-				t++;
-			} else {
-				console.log("mistake");
-			}
-		}
-	}
+    const keys = {
+        BACKSPACE: 8,
+
+    }
+
+    console.log("start");
+
+    let words = [
+        "bother",
+        "elephant",
+        "panel",
+        "disability",
+        "inch",
+        "pyramid",
+        "separate",
+        "nationalism",
+        "live",
+        "rush",
+        "disappointment",
+        "salt",
+        "file",
+        "substitute",
+        "green",
+        "assignment",
+        "garage",
+        "horse",
+        "default",
+        "favorable",
+        "penetrate",
+        "version",
+        "velvet",
+        "allocation",
+        "pleasure",
+        "testify",
+        "touch",
+        "functional",
+        "Koran",
+        "direction",
+    ];
+    let token = "";
+
+    let wordIndex = 0;
+    let letterIndex = 0;
+    let mistakeIndex = 0;
+
+    function onInput(e) {
+
+        console.log(e);
+
+        const word = words[wordIndex];
+
+        if (e.data === " ") {
+            const tokenWithoutSpace = token.substr(0, token.length - 1);
+            if (tokenWithoutSpace === word) {
+                wordIndex++;
+                letterIndex = 0;
+                token = "";
+            }
+            else mistakeIndex++;
+        } else {
+            letterIndex = 0;
+            mistakeIndex = 0;
+            for (let i = 0; i < token.length; i++) {
+                if (word[i] && word[i] === token[i]) {
+                    letterIndex++;
+                } else {
+                    mistakeIndex++;
+                }
+            }
+        }
+
+        console.log(wordIndex, letterIndex, mistakeIndex);
+
+    }
 </script>
 
+<main>
+    <div class="wrapper">
+        <div class="words">
+            {#each words as word}
+                <Word {word} correct={[]} />
+            {/each}
+        </div>
+        <input bind:value={token} on:input={onInput} />
+        <p>Typed: {token}</p>
+    </div>
+</main>
 
 <style>
+    main {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
+    .wrapper {
+        width: 50%;
+    }
+
+    .words {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
 </style>
-
-
-{#each words as word}
-	<Word word={word} correct={[]} />
-{/each}
-<input bind:value={typed} on:input={onInput}>
-<p>Typed: {typed}</p>
-
--- src/Word.svelte
+++ src/Word.svelte
@@ -1,14 +1,32 @@
 <script lang="ts">
     export let word: string;
     export let correct: Boolean[];
+
+    function getClass(i) {
+        const c = correct[i];
+        if (c === null || c === undefined) return "";
+        else if (c) return "green";
+        else return "red";
+    }
 </script>
 
 
 <style>
+    .word {
+        margin-right: 0.25em;
+    }
+    .red {
+        color: red;
+    }
 
+    .green {
+        color: green;
+    }
 </style>
 
 
+<span class="word">
 {#each word as c, i}
-    <span class={correct[i] ? "green" : "red"}>{c}</span>
-{/each}
+    <span class={getClass(i)}>{c}</span>
+    {/each}
+</span>
